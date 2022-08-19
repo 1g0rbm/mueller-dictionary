48b79c895d0c60f9a3f33dceaaf050a8f8e2cf07
1g0rbm <m1g0rb89@gmail.com>
2022-08-20 00:17:16 +0300

    Added text splitting on words

---
+++ src/Service/WordsReader.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use function fgets;
+use function implode;
+use function trim;
+
+final class WordsReader
+{
+    /**
+     * @param resource $fp
+     *
+     * @return string[]
+     */
+    public function read($fp, int $count = 10): array
+    {
+        $words = [];
+        /** @var string[] $word */
+        $word = [];
+        while (!feof($fp) && \count($words) < $count) {
+            $line = self::readLine($fp);
+            if (!$line) {
+                continue;
+            }
+
+            if ($line === '_____') {
+                $words = self::saveWord($words, $word);
+                continue;
+            }
+
+            $word[] = $line;
+        }
+
+        /** @psalm-var string[] */
+        return $words;
+    }
+
+    /**
+     * @param string[] $acc
+     * @param string[] $word
+     *
+     * @return string[]
+     */
+    private function saveWord(array $acc, array $word): array
+    {
+        if (empty($word)) {
+            return $acc;
+        }
+
+        $acc[] = implode(' ', $word);
+
+        return $acc;
+    }
+
+    /**
+     * @param resource $fp
+     */
+    private function readLine($fp): false|string
+    {
+        $line = fgets($fp);
+
+        if (!$line) {
+            return false;
+        }
+
+        return trim($line);
+    }
+}
---
+++ tests/Service/WordsReaderUnitTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Service;
+
+use App\Service\WordsReader;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+use function fopen;
+
+/**
+ * @internal
+ */
+final class WordsReaderUnitTest extends KernelTestCase
+{
+    private WordsReader $service;
+
+    protected function setUp(): void
+    {
+        $this->service = new WordsReader();
+    }
+
+    public function testReadLinesByCount(): void
+    {
+        $fp    = fopen(__DIR__ . '/fixtures/five_words', 'rb');
+        $words = $this->service->read($fp, 2);
+
+        self::assertCount(2, $words);
+        self::assertEquals("beehive ['bi:haIv] _n. улей", $words[0]);
+    }
+
+    public function testReadLinesToEnd(): void
+    {
+        $fp    = fopen(__DIR__ . '/fixtures/five_words', 'rb');
+        $words = $this->service->read($fp);
+
+        self::assertCount(5, $words);
+        self::assertEquals("beehive ['bi:haIv] _n. улей", $words[0]);
+    }
+
+    public function testReadLinesPartials(): void
+    {
+        $fp    = fopen(__DIR__ . '/fixtures/five_words', 'rb');
+        $words = $this->service->read($fp, 1);
+
+        self::assertCount(1, $words);
+        self::assertEquals("beehive ['bi:haIv] _n. улей", $words[0]);
+
+        $words = $this->service->read($fp, 1);
+
+        self::assertCount(1, $words);
+        self::assertEquals('been [bi:n] (полная форма); [bIn] (редуцированная форма) _p-p. от be', $words[0]);
+    }
+}
